3178e8c1e316217c5e309a590563f13fa12f17cd
Isaac Yawson <isaacayawson1996@gmail.com>
2025-07-17 10:43:23 -0700

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class RequestLog(models.Model):
-    ip_address = models.GenericIPAddressField()  # 2. Stores IPv4 or IPv6
-    timestamp = models.DateTimeField(auto_now_add=True)  # 3. Automatically saves current time
-    path = models.CharField(max_length=200)  # 4. Path the user accessed
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=200)
+
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.ip_address} - {self.path} at {self.timestamp}"
