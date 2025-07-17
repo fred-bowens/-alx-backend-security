2d5b4dfb81649dbf4f627b94efc9c81337a3e764
Isaac Yawson <isaacayawson1996@gmail.com>
2025-07-17 10:30:21 -0700

    Create middleware.py

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,22 @@
+from .models import RequestLog
+from django.utils.timezone import now
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0].strip()  
+        else:
+            ip = request.META.get('REMOTE_ADDR')  
+
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=request.path,
+            timestamp=now()
+        )
+
+        response = self.get_response(request)
+        return response
