d42688d54b4b37eefa7f5fcdfaa3f1eaf9b7f5bc
Isaac Yawson <isaacayawson1996@gmail.com>
2025-07-17 10:46:10 -0700

    Update middleware.py

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponseForbidden
-from .models import BlockedIP, RequestLog
 from django.utils.timezone import now
+from django.http import HttpResponseForbidden
+from django.core.cache import cache
+from ipgeolocation import IpGeoLocation
+from .models import RequestLog, BlockedIP
+
+geo_locator = IpGeoLocation()
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -16,10 +20,29 @@ class RequestLoggingMiddleware:
         if BlockedIP.objects.filter(ip_address=ip).exists():
             return HttpResponseForbidden("Your IP has been blocked.")
 
+        cache_key = f"geo_{ip}"
+        geo_info = cache.get(cache_key)
+
+        if not geo_info:
+            try:
+                geo_data = geo_locator.get(ip)
+
+                country = geo_data.get('country_name', '')
+                city = geo_data.get('city', '')
+
+                geo_info = {'country': country, 'city': city}
+                cache.set(cache_key, geo_info, timeout=86400)
+
+            except Exception as e:
+                
+                geo_info = {'country': '', 'city': ''}
+
         RequestLog.objects.create(
             ip_address=ip,
             path=request.path,
-            timestamp=now()
+            timestamp=now(),
+            country=geo_info['country'],
+            city=geo_info['city']
         )
 
         return self.get_response(request)
