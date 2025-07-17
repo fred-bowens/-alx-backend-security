aab85c00254de331551a10d6a6f606a1db9634c1
Isaac Yawson <isaacayawson1996@gmail.com>
2025-07-17 10:55:27 -0700

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -8,5 +8,10 @@ class RequestLog(models.Model):
     country = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
 
+class SuspiciousIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)  # 2. IP address that triggered suspicion
+    reason = models.TextField()  # 3. Explanation of why this IP was flagged
+    flagged_at = models.DateTimeField(auto_now_add=True) 
+
     def __str__(self):
-        return f"{self.ip_address} - {self.path} at {self.timestamp}"
+        return f"{self.ip_address} - {self.path/reason} at {self.timestamp}"
